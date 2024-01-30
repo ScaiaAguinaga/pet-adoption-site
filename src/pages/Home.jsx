83711d2a1c737d41985c9eb1f66b4ab74778102f
Isaac <Scaiaaguinaga@gmail.com>
2024-01-29 20:10:41 -0800

    Finished working on hero section buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,38 @@
 import "../styles.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      {/* hero section */}
+      {/* Hero Section */}
       <div className="flex h-[440px] flex-col items-center justify-center gap-10 bg-hero-image bg-cover bg-center">
-        <inpu className="mt-10 h-10 w-1/3 rounded-[20px] border-2 border-cerulean bg-white"></inpu>
+        <input className="mt-10 h-10 w-1/3 rounded-[20px] border-2 border-cerulean bg-white"></input>
         <div className="text-offwhite text-3xl">Find Your Forever Friend</div>
       </div>
-      <div className="grid h-3 grid-cols-4 gap-5 bg-cerulean">
-        <button className="flex h-[150px] rounded-[20px] bg-red-500">Adopt</button>
-        <button className="flex h-[150px] rounded-[20px] bg-red-500">Rehome</button>
-        <button className="flex h-[150px] rounded-[20px] bg-red-500">Volunteer</button>
-        <button className="flex h-[150px] rounded-[20px] bg-red-500">Contact</button>
+      {/* Hero Section Buttons */}
+      <div className="mb-36 flex h-3 justify-center bg-cerulean">
+        <Link to="/Test">
+          <button className="text-offwhite hover:border-offwhite relative bottom-12 mx-2 flex h-[125px] w-[150px] items-center justify-center rounded-[20px] bg-cerulean hover:border-2">
+            Adopt
+          </button>
+        </Link>
+        <Link to="/Test">
+          <button className="text-offwhite hover:border-offwhite relative bottom-12 mx-2 flex h-[125px] w-[150px] items-center justify-center rounded-[20px] bg-cerulean hover:border-2">
+            Rehome
+          </button>
+        </Link>
+        <Link to="/Test">
+          <button className="text-offwhite hover:border-offwhite relative bottom-12 mx-2 flex h-[125px] w-[150px] items-center justify-center rounded-[20px] bg-cerulean hover:border-2">
+            Volunteer
+          </button>
+        </Link>
+        <Link to="/Test">
+          <button className="text-offwhite hover:border-offwhite relative bottom-12 mx-2 flex h-[125px] w-[150px] items-center justify-center rounded-[20px] bg-cerulean hover:border-2">
+            Contact
+          </button>
+        </Link>
       </div>
+      {/* New Pals */}
     </>
   );
 };
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,19 +1,13 @@
 import "../styles.css";
+import { Link } from "react-router-dom";
 
 const Test = () => {
   return (
-    <>
-      {/* hero section */}
-      <div className="flex h-[440px] flex-col items-center justify-center gap-10 bg-hero-image bg-cover bg-center">
-        {/* hero section search bar */}
-        <div className="mt-20 flex h-12 w-1/3 items-center justify-center rounded-[20px] border-2 border-cerulean bg-white">
-          <div>Put Magnifying Glass</div>
-        </div>
-        {/* call to action */}
-        <div className="text-[40px] text-white">Find Your Forever Friend</div>
-      </div>
-      <div className="flex h-3 grid-cols-4 justify-center gap-10 bg-cerulean"></div>
-    </>
+    <div className="text-offwhite h-40 w-40 bg-red-500 rounded-[20px] m-auto flex">
+      <Link to="/" className="w-full h-full flex items-center justify-center">
+        Back Home
+      </Link>
+    </div>
   );
 };
 
