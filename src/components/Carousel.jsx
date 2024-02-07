886b30aa53e27249e7fb04d0cc0e0bea998a6499
Isaac <Scaiaaguinaga@gmail.com>
2024-02-06 21:39:57 -0800

    Completed the carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,58 @@
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { ChevronLeft, ChevronRight } from "react-feather";
 
-const Carousel = ({ children: slides }) => {
+const Carousel = ({
+  children: slides,
+  autoSlide = false,
+  autoSlideInterval = 2000,
+}) => {
+  const [curr, setCurr] = useState(0);
+
+  const prev = () =>
+    setCurr((curr) => (curr == 0 ? slides.length - 1 : curr - 1));
+
+  const next = () =>
+    setCurr((curr) => (curr == slides.length - 1 ? 0 : curr + 1));
+
+  useEffect(() => {
+    if (!autoSlide) return;
+    const SlideInterval = setInterval(next, autoSlideInterval);
+    return () => clearInterval(SlideInterval);
+  });
+
   return (
     <div className="relative overflow-hidden">
-      <div className="flex ">{slides}</div>
-      <div>
-        {/* <button>
-          <ChevronLeft className="size-10"/>
+      <div
+        className="flex transition-transform duration-500 ease-out"
+        style={{ transform: `translateX(-${curr * 100}%)` }}
+      >
+        {slides}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-4">
+        <button
+          onClick={prev}
+          className="rounded-full bg-offwhite/80 p-1 text-gray-800 shadow hover:bg-offwhite"
+        >
+          <ChevronLeft className="size-10" />
         </button>
-        <button>
-          <ChevronRight className="size-10"/>
-        </button> */}
+        <button
+          onClick={next}
+          className="rounded-full bg-offwhite/80 p-1 text-gray-800 shadow hover:bg-offwhite"
+        >
+          <ChevronRight className="size-10" />
+        </button>
+      </div>
+
+      <div className="absolute bottom-4 left-0 right-0">
+        <div className="flex items-center justify-center gap-2">
+          {slides.map((_, i) => (
+            <div
+              key={i} // Added key prop to each div element
+              className={`h-3 w-3 rounded-full bg-offwhite transition-all ${curr === i ? "p-2" : "bg-opacity-50"}`}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -22,4 +63,6 @@ export default Carousel;
 // PropTypes validation
 Carousel.propTypes = {
   children: PropTypes.array.isRequired, // 'slides' prop is expected to be an array and is required
+  autoSlide: PropTypes.bool.isRequired,
+  autoSlideInterval: PropTypes.number.isRequired,
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,46 +14,45 @@ const Home = () => {
   return (
     <>
       {/* Hero Section */}
-      <div className="flex h-[440px] flex-col items-center justify-center gap-10 bg-hero-image bg-cover bg-center">
-        <input className="mt-10 h-10 w-1/3 rounded-[20px] border-2 border-cerulean bg-white"></input>
-        <div className="text-3xl text-offwhite">Find Your Forever Friend</div>
-      </div>
-      {/* Hero Section Buttons */}
-      <div className="mb-36 flex h-3 justify-center bg-cerulean">
-        <HeroButton to="/Test" name="Adopt" />
-        <HeroButton to="/Test" name="Rehome" />
-        <HeroButton to="/Test" name="Volunteer" />
-        <HeroButton to="/Test" name="Contact" />
-      </div>
+      <section className="mb-36">
+        {/* Background and slogan */}
+        <div className="flex h-[440px] flex-col items-center justify-center gap-10 bg-hero-image bg-cover bg-center">
+          <input className="mt-10 h-10 w-1/3 rounded-[20px] border-2 border-cerulean bg-white"></input>
+          <h1 className="text-3xl text-offwhite">Find Your Forever Friend</h1>
+        </div>
+        {/* Main page buttons */}
+        <div className="flex h-3 justify-center bg-cerulean">
+          <HeroButton to="/Test" name="Adopt" />
+          <HeroButton to="/Test" name="Rehome" />
+          <HeroButton to="/Test" name="Volunteer" />
+          <HeroButton to="/Test" name="Contact" />
+        </div>
+      </section>
       {/* New Pals */}
-      <div className="flex justify-center">New Pals</div>
+      <h2 className="flex justify-center">New Pals</h2>
       {/* New Pals carousel */}
-      {/* Need to move this over to the carousel component */}
-      {/* <div className="m-auto flex h-[450px] w-[850px] rounded-[20px] border bg-blue-300 p-4 ">
-        <div className="grid h-full w-full grid-cols-3 gap-6 rounded-[20px] bg-red-400">
-          <div className="flex items-center justify-center overflow-hidden rounded-[20px] bg-green-400">
-            <Carousel>
-              {slides.map((slide, index) => (
-                <img key={index} src={slide} />
-              ))}
-            </Carousel>
-          </div>
-          <div className="col-span-2 justify-center bg-purple-500 py-5">
-            <div className="border text-center">Name</div>
-            <div className="border">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id error
-              ducimus enim repellendus suscipit minus omnis iure officia eum
-              impedit illum, tenetur nisi similique accusamus quia ipsam quam
-              soluta voluptas!
+      <section>
+        <div className="m-auto flex h-[450px] w-[850px] rounded-[20px] border bg-blue-300 p-4 ">
+          <div className="grid h-full w-full grid-cols-3 gap-6 rounded-[20px] bg-red-400">
+            <div className="flex items-center justify-center overflow-hidden rounded-[20px] bg-green-400">
+              <Carousel autoSlide={true}>
+                {slides.map((slide, index) => (
+                  <img key={index} src={slide} />
+                ))}
+              </Carousel>
+            </div>
+            <div className="col-span-2 justify-center bg-purple-500 py-5">
+              <h3 className="border text-center">Name</h3>
+              <p className="border">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id
+                error ducimus enim repellendus suscipit minus omnis iure officia
+                eum impedit illum, tenetur nisi similique accusamus quia ipsam
+                quam soluta voluptas!
+              </p>
             </div>
           </div>
         </div>
-      </div> */}
-      <Carousel>
-        {slides.map((slide, index) => (
-          <img key={index} src={slide} />
-        ))}
-      </Carousel>
+      </section>
     </>
   );
 };
