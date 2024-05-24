a95c96a2e926224912e18ca1ba87904269dcb682
Isaac <Scaiaaguinaga@gmail.com>
2024-05-23 23:11:11 -0700

    basis for adoption page

--- src/pages/Adopt.jsx
+++ src/pages/Adopt.jsx
@@ -1,5 +1,24 @@
 const Adopt = () => {
-  return <div>Adopt</div>;
+  return (
+    <div className="flex w-1/2 justify-center">
+      <div className="grid grid-cols-4">
+        {/* Column for search filters */}
+        <div className="col-span-1">
+          <div>settings for filter</div>
+        </div>
+        <div className="col-span-3 grid grid-cols-3">
+          <div className="h-20 w-20 bg-blue-500"></div>
+          <div className="h-20 w-20 bg-red-500"></div>{" "}
+          <div className="h-20 w-20 bg-blue-500"></div>
+          <div className="h-20 w-20 bg-red-500"></div>
+          <div className="h-20 w-20 bg-blue-500"></div>
+          <div className="h-20 w-20 bg-red-500"></div>{" "}
+          <div className="h-20 w-20 bg-blue-500"></div>
+          <div className="h-20 w-20 bg-red-500"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Adopt;
