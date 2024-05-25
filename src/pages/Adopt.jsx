58a82a77a782490af3db7b3871e92cdd0d1ed1aa
Isaac <Scaiaaguinaga@gmail.com>
2024-05-24 21:35:38 -0700

    made progress on layout of adoption page

--- src/pages/Adopt.jsx
+++ src/pages/Adopt.jsx
@@ -1,22 +1,39 @@
 const Adopt = () => {
   return (
-    <div className="flex min-h-screen justify-center">
-      <div className="grid w-full grid-cols-4">
+    <div className="flex justify-center">
+      <div className="grid w-1/2 grid-cols-4">
+        <div className="col-span-4 justify-center h-96"> Filler Content </div>
         {/* Column for search filters */}
         <div className="col-span-1">
-          <div className="sticky top-0 bg-white p-4 shadow-md">
-            settings for filter
+          <div className="sticky top-16 flex h-80 justify-center">
+            <div>filter</div>
           </div>
         </div>
-        <div className="col-span-3 grid h-screen grid-cols-3 gap-4 overflow-auto">
-          <div className="h-20 w-20 bg-blue-500"></div>
-          <div className="h-20 w-20 bg-red-500"></div>
-          <div className="h-20 w-20 bg-blue-500"></div>
-          <div className="h-20 w-20 bg-red-500"></div>
-          <div className="h-20 w-20 bg-blue-500"></div>
-          <div className="h-20 w-20 bg-red-500"></div>
-          <div className="h-20 w-20 bg-blue-500"></div>
-          <div className="h-20 w-20 bg-red-500"></div>
+        <div className="col-span-3 grid grid-cols-3 gap-8 justify-self-center overflow-auto">
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
+          <div className=" h-[350px] w-[250px] border-2 border-black bg-offwhite">
+            <div className="h-2/3 bg-red-500">picture</div>
+            <div className="h-1/3 bg-blue-500">name</div>
+          </div>
         </div>
       </div>
     </div>
